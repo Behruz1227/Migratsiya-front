a84f72beab09761e5a25f544e17ae92fcb637957
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-12-15 18:27:27 +0500

    er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true, // Container markazda bo'ladi
+        padding: {
+          DEFAULT: '20px', // Default padding
+          sm: '1rem',      // Kichik ekranlar uchun padding
+          md: '2rem',      // O'rta ekranlar uchun padding
+          lg: '4rem',      // Katta ekranlar uchun padding
+          xl: '5rem',      // Juda katta ekranlar uchun padding
+        },
+      },
+    },
   },
   plugins: [],
 };

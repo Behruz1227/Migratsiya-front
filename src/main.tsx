2a9ef74577fcda866d0d21c39ba3173efc50a508
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-12-15 14:01:45 +0500

    ew

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import Navbar from "./components/navbar/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
+      <Navbar navigation={[{name: "Home", path: "/home"}]} />
       <App />
+      <h1 className="text-green-700 text-3xl">asaaassasasasasa</h1>
     </StrictMode>
   </BrowserRouter>
 );

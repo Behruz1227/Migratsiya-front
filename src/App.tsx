ce41d679e3fba155db770067eefc65e69ee9e3e7
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-12-15 12:36:09 +0500

    ewew

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="bg-blue-500 text-white text-center p-10">
+      <h1 className="text-3xl font-bold">Tailwind CSS Vite Loyihasi!</h1>
+    </div>
   )
 }
 

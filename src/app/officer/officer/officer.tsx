4d8523ea2a949cc74cc5fc0d83452a59cac4062b
Omadbek <omadbekolimov96@gmail.com>
2024-12-15 19:56:08 +0500

    office

--- src/app/officer/officer/officer.tsx
+++ src/app/officer/officer/officer.tsx
@@ -4,6 +4,8 @@ import { BsFillFilterSquareFill } from "react-icons/bs";
 import Tables, { IThead } from "../../../components/table";
 import DateInput from "../../../components/inputs/date-input";
 import TextInput from "../../../components/inputs/text-input";
+import { Tab } from "../../../helpers/constants/types";
+import TabsMigrant from "../../../components/tabs/tab";
 
 // Assuming Input is a custom component
 const Input: React.FC<any> = ({ name, placeholder, value, onChange, onKeyDown, color, onFilterClick }) => (
@@ -27,7 +29,10 @@ const Officer: React.FC = () => {
     const [inputValue, setInputValue] = useState('');
     const [inputTextColor] = useState('text-black');
     const [filterVisible, setFilterVisible] = useState(false); // New state to toggle filter visibility
+    const [name, setName] = useState(''); // New state for filter input value
+    const [fullName, setfullName] = useState(''); // New state for filter input value
     const [filterValue, setFilterValue] = useState(''); // New state for filter input value
+    const [fatherName, setfatherName] = useState(''); // New state for filter input value
     const [filterDate, setFilterDate] = useState(''); // New state for date filter value
 
     const tableHeaders: IThead[] = [
@@ -69,6 +74,66 @@ const Officer: React.FC = () => {
 
         return matchesText && matchesDate;
     });
+
+    const tabs: Tab[] = [
+        {
+            id: 1, title: "Migrant  qo’shish", content:
+                <div className="flex space-x-16 mt-6">
+                    <TextInput
+                        label="Ismi"
+                        value={name}
+                        type="text"
+                        handleChange={handleFilterChange}
+                        placeholder="Ismi"
+                    />
+                    <TextInput
+                        label="Familiyasi"
+                        value={fullName}
+                        type="text"
+                        handleChange={handleFilterChange}
+                        placeholder="Familiyasi"
+                    />
+                    <TextInput
+                        label="Otasini ismi"
+                        value={fatherName}
+                        type="text"
+                        handleChange={handleFilterChange}
+                        placeholder="Otasini ismi"
+                    />
+                    <DateInput
+                        label="Tug’ilgan sanasi"
+                        value={filterDate}
+                        handleChange={handleDateChange}
+                        placeholder="Select date"
+                    />
+                </div>
+        },
+        {
+            id: 2, title: "Horijdagi Migrantlar", content:
+                <div className="mt-6">
+                    <Tables thead={tableHeaders}>
+                        {filteredData.map((item) => (
+                            <tr key={item.id} className="hover:bg-blue-300 border-b">
+                                <td className="p-5">{item.fio}</td>
+                                <td className="p-5">{item.tel}</td>
+                                <td className="p-5">{item.createdDate}</td>
+                                <td className="p-5 flex justify-center space-x-4">
+                                    <button className="text-blue-500 hover:text-blue-700">
+                                        <FaEdit />
+                                    </button>
+                                    <button className="text-red-500 hover:text-red-700">
+                                        <FaTrash />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </Tables>
+                </div>
+        },
+        { id: 3, title: "Qaytib kelganini ro’yhatga olish", content: "Qashqadaryo bo‘yicha ma'lumotlar" },
+
+
+    ];
 
     return (
         <div className="flex justify-center min-h-screen bg-gray-100">
@@ -146,25 +211,7 @@ const Officer: React.FC = () => {
                         </div>
                     )}
                 </div>
-                <div className="mt-6">
-                    <Tables thead={tableHeaders}>
-                        {filteredData.map((item) => (
-                            <tr key={item.id} className="hover:bg-blue-300 border-b">
-                                <td className="p-5">{item.fio}</td>
-                                <td className="p-5">{item.tel}</td>
-                                <td className="p-5">{item.createdDate}</td>
-                                <td className="p-5 flex justify-center space-x-4">
-                                    <button className="text-blue-500 hover:text-blue-700">
-                                        <FaEdit />
-                                    </button>
-                                    <button className="text-red-500 hover:text-red-700">
-                                        <FaTrash />
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </Tables>
-                </div>
+                <TabsMigrant tabs={tabs} />
             </div>
         </div>
     );

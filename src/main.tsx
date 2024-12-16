c1e7f96dbdf0f2c2b26404e23e506c27e9948eae
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-12-16 16:35:00 +0500

    wq

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <Toaster richColors />
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <StrictMode>
+        <Toaster richColors />
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </QueryClientProvider>
 );

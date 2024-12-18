2f47549a35d7ea3a3a592070b5a02b6c1474c492
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-12-18 14:11:26 +0500

    sa

---
+++ src/components/loading/loadingDiv.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoadingDiv: React.FC = () => {
+  return (
+    <div
+      className={`rounded-lg border border-[#0086D1] flex justify-center items-center p-4 transition-colors ${"bg-white hover:shadow-md"}`}
+    >
+      Loading...
+      
+    </div>
+  );
+};
+
+export default LoadingDiv;

e0d87780e3d7ad091194841c7a0a5de9c503a995
Alisher0903 <alishersodiqov09@gmail.com>
2025-01-16 14:13:59 +0500

    katta oficer xam panel full

--- src/helpers/api/api.tsx
+++ src/helpers/api/api.tsx
@@ -1,78 +1,77 @@
-// const BASE_URL = "http://161.35.214.247:7777/"
-const BASE_URL = "http://185.100.55.152:8080/"
+export const base_url: string = import.meta.env.VITE_BASE_URL;
 
-export const log_in = `${BASE_URL}api/auth/login`
+export const log_in = `${base_url}api/auth/login`
 
 // Uchaskavoy
-export const addManager = `${BASE_URL}api/auth/register`
-export const editManager = `${BASE_URL}api/auth/edit/by/admin`
-export const deleteManager = `${BASE_URL}api/user`
-export const getManager = `${BASE_URL}api/user/users`
+export const addManager = `${base_url}api/auth/register`
+export const editManager = `${base_url}api/auth/edit/by/admin`
+export const deleteManager = `${base_url}api/user`
+export const getManager = `${base_url}api/user/users`
 
 // Admin 
-export const addUser = `${BASE_URL}api/auth/register`
-export const getUser = `${BASE_URL}api/user/admins`
+export const addUser = `${base_url}api/auth/register`
+export const getUser = `${base_url}api/user/admins`
 
 //Uchaskavoy qo'shguncha tuman listi 
-export const getTuman = `${BASE_URL}api/district/list-qa`
-export const editUchaskavoy = `${BASE_URL}api/auth/update/uchaskavoy`
-export const mfyList = `${BASE_URL}api/mfy/list`
+export const getTuman = `${base_url}api/district/list-qa`
+export const editUchaskavoy = `${base_url}api/auth/update/uchaskavoy`
+export const mfyList = `${base_url}api/mfy/list`
 
 // Kichik uchaskavoy qo'shish 
-export const getMfy = `${BASE_URL}api/mfy/districtBy`
-export const postUchaskavoy = `${BASE_URL}api/auth/add-uchaskavoy`
-export const getKichikUchaskavoy = `${BASE_URL}api/user/uchaskavoy-list`
+export const getMfy = `${base_url}api/mfy/districtBy`
+export const postUchaskavoy = `${base_url}api/auth/add-uchaskavoy`
+export const getKichikUchaskavoy = `${base_url}api/user/uchaskavoy-list`
 
 //uchaskavoy migrate qo'shadi
-export const addMigrate = `${BASE_URL}api/migrate`
-export const getMigrate = `${BASE_URL}api/migrate/search`
-export const editMigrate = `${BASE_URL}api/migrate`
-export const deleteMigrate = `${BASE_URL}api/migrate`
-export const MFYList = `${BASE_URL}mfy/list`
+export const addMigrate = `${base_url}api/migrate`
+export const getMigrate = `${base_url}api/migrate/search`
+export const editMigrate = `${base_url}api/migrate`
+export const deleteMigrate = `${base_url}api/migrate`
+export const MFYList = `${base_url}mfy/list`
 
 // IMG API
-export const postImg = `${BASE_URL}api/attachment/upload`
-export const imgUpdate = `${BASE_URL}api/attachment/`
-export const imgGet = `${BASE_URL}api/attachment/getFile/`
+export const postImg = `${base_url}api/attachment/upload`
+export const imgUpdate = `${base_url}api/attachment/`
+export const imgGet = `${base_url}api/attachment/getFile/`
 
 //User API
-export const getUserInfo = `${BASE_URL}api/user/getMe`
-export const editUserInfo = `${BASE_URL}api/auth/edit/profile`
+export const getUserInfo = `${base_url}api/user/getMe`
+export const editUserInfo = `${base_url}api/auth/edit/profile`
 
 // diagram 
-export const getYear = `${BASE_URL}api/statistic/diagram/gone`
-export const getLeave = `${BASE_URL}api/statistic/diagram/gone`
-export const getArrive = `${BASE_URL}api/statistic/diagram/arrive`
-export const getMigratesStatistic = `${BASE_URL}api/statistic/diagram/by/migrant-count`
-export const getStatisByRegion = `${BASE_URL}api/statistic/diagram/by/district`
-export const getStatisForBarChartByCountry = `${BASE_URL}api/statistic/ageBy/country`
-export const getStatisForBarChartByDistrict = `${BASE_URL}api/statistic/ageBy/district`
-export const getStatisticByCountry = `${BASE_URL}api/statistic/statistics/country/by?`
+export const getYear = `${base_url}api/statistic/diagram/gone`
+export const getLeave = `${base_url}api/statistic/diagram/gone`
+export const getArrive = `${base_url}api/statistic/diagram/arrive`
+export const getMigratesStatistic = `${base_url}api/statistic/diagram/by/migrant-count`
+export const getStatisByRegion = `${base_url}api/statistic/diagram/by/district`
+export const getStatisForBarChartByCountry = `${base_url}api/statistic/ageBy/country`
+export const getStatisForBarChartByDistrict = `${base_url}api/statistic/ageBy/district`
+export const getStatisticByCountry = `${base_url}api/statistic/statistics/country/by?`
 
 // Dashboard api
-export const get_country = `${BASE_URL}api/statistic/migrants/by/country`
-export const get_region = `${BASE_URL}api/statistic/migrants/by/country/regions`
-export const get_user_by_country = `${BASE_URL}api/migrate/departure/region`
-export const all_migrants = `${BASE_URL}api/statistic/all/migrant/count`
-export const statistic_by_kashkadarya = `${BASE_URL}api/statistic/by/kashkadarya`
-export const migrates_by_kashkadarya = `${BASE_URL}api/migrate/district/migrants`
-export const migrates_last_3month = `${BASE_URL}api/migrate/last/three/month`
-export const statistic_last_3month = `${BASE_URL}api/statistic/last/three/month`
-export const migrates_now_uzb = `${BASE_URL}api/migrate/now/at/uzb`
-export const statistic_now_uzb = `${BASE_URL}api/statistic/now/at/uzb`
-export const get_brigader = `${BASE_URL}api/statistic/migrants/by/brigaderlar`
-export const get_brigader_by_country = `${BASE_URL}api/statistic/brigadir/by/country/regions`
-export const get_brigader_users = `${BASE_URL}api/migrate/brigader/region`
-export const get_brigader_count = `${BASE_URL}api/statistic/now/at/brigadir`
-export const get_searchM_by_country = `${BASE_URL}api/statistic/qidiruv/by/country/regions`
-export const get_searchM = `${BASE_URL}api/statistic/migrants/by/qidiruv`
-export const get_searchM_count = `${BASE_URL}api/statistic/now/at/qidiruvda`
-export const get_searchM_users = `${BASE_URL}api/migrate/qidiruv/region`
+export const get_country = `${base_url}api/statistic/migrants/by/country`
+export const get_region = `${base_url}api/statistic/migrants/by/country/regions`
+export const get_user_by_country = `${base_url}api/migrate/departure/region`
+export const all_migrants = `${base_url}api/statistic/all/migrant/count`
+export const statistic_by_kashkadarya = `${base_url}api/statistic/by/kashkadarya`
+export const migrates_by_kashkadarya = `${base_url}api/migrate/district/migrants`
+export const migrates_last_3month = `${base_url}api/migrate/last/three/month`
+export const statistic_last_3month = `${base_url}api/statistic/last/three/month`
+export const migrates_now_uzb = `${base_url}api/migrate/now/at/uzb`
+export const statistic_now_uzb = `${base_url}api/statistic/now/at/uzb`
+export const get_brigader = `${base_url}api/statistic/migrants/by/brigaderlar`
+export const get_brigader_by_country = `${base_url}api/statistic/brigadir/by/country/regions`
+export const get_brigader_users = `${base_url}api/migrate/brigader/region`
+export const get_brigader_count = `${base_url}api/statistic/now/at/brigadir`
+export const get_searchM_by_country = `${base_url}api/statistic/qidiruv/by/country/regions`
+export const get_searchM = `${base_url}api/statistic/migrants/by/qidiruv`
+export const get_searchM_count = `${base_url}api/statistic/now/at/qidiruvda`
+export const get_searchM_users = `${base_url}api/migrate/qidiruv/region`
 
 // country list 
-export const countryList = `${BASE_URL}api/country/list`
-export const regionList = `${BASE_URL}api/region/list`
-export const distList = `${BASE_URL}api/district/list`
-export const distListByQa = `${BASE_URL}api/district/list-qa`
+export const countryList = `${base_url}api/country/list`
+export const regionList = `${base_url}api/region/list`
+export const distList = `${base_url}api/district/list`
+export const distListByQa = `${base_url}api/district/list-qa`
 
-export const getVillage = `${BASE_URL}api/auth/small/police/village`
+export const getVillage = `${base_url}api/auth/small/police/village`
